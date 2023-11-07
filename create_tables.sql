2af3920fd7f7c9d21e9f0d91cbf5a78af215aea4
Callan <callan.v@fake.com>
2023-11-07 14:15:10 +1100

    Completed first lesson on postgres and SQL

--- create_tables.sql
+++ create_tables.sql
@@ -1,12 +1,17 @@
-drop table categories;
 drop table items;
+drop table categories;
 
 create table  categories (
     id serial primary key,
 
-    name varchar(50) not null,
+    name varchar(50) not null unique,
     description text
 );
+
+insert into categories (name, description) values
+    ('Electronics', 'Fun things!'),
+    ('Car Parts', 'Expensive things!')
+;
 
 create table  items (
     id serial primary key,
@@ -17,3 +22,4 @@ create table  items (
     categories_id integer not null,
     foreign key (categories_id) references categories (id)
 );
+
